cacb28069b4dfbabde2550d07e6ea421dcabf6d4
cvpcs <root@cvpcs.org>
2010-11-30 14:03:03 -0600

    base commit

---
+++ src/org/cvpcs/android/cwiidconfig/activity/CWiiDConfig.java
@@ -0,0 +1,20 @@
+package org.cvpcs.android.cwiidconfig.activity;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+
+import org.cvpcs.android.cwiidconfig.R;
+
+/**
+ * Settings for CWiiD wiimote connectivity
+ *
+ * @author cvpcs, nehresmann
+ */
+public final class CWiiDConfig extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
